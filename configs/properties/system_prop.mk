78c1692a773cdfad5b5e84d32b164683417bc115
johnmart19 <johnivan19999@gmail.com>
2022-05-19 17:32:06 +0530

    spaced: properties: Define support for HDR and WCD support
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>
    Signed-off-by: rk134 <spaced@rk134.cf>

--- configs/properties/system_prop.mk
+++ configs/properties/system_prop.mk
@@ -72,7 +72,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     debug.sf.latch_unsignaled=1 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.has_HDR_display=true
 
 # Zygote
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
