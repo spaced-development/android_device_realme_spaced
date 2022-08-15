54925749ca541c89db7b11cf1fab6cafe1757d7b
hpnightowl <iamhp2k@gmail.com>
2022-08-16 05:23:56 +0530

    spaced: Drop audio.a2dp.default
    * Building from here:
      https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/full_base.mk#L35
    
    Change-Id: Ib71c1abd381468ec3621d60e5ca959f738f51168
    Signed-off-by: rk134 <spaced@rk134.cf>

--- device.mk
+++ device.mk
@@ -32,10 +32,6 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
 
-# Audio
-PRODUCT_PACKAGES += \
-    audio.a2dp.default
-
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_EXTRA_VNDK_VERSIONS)/etc/audio_policy_configuration.xml
 
