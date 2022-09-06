9ef8cdffd4a5020b081d0959e9629fed59569968
rk134 <spaced@rk134.cf>
2022-09-06 20:18:43 +0100

    spaced: BoardConfig: organize + comment sections
    Signed-off-by: rk134 <spaced@rk134.cf>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
-<#
+#
 # Copyright (C) 2021 Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,6 +14,7 @@
 # limitations under the License.
 #
 
+# Device path
 DEVICE_PATH := device/realme/spaced
 
 # Architecture
@@ -24,6 +25,7 @@ TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a78
 
+# Second architecture
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
@@ -31,14 +33,18 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a78
 
-# Android Verified Boot
+# Android Verified Boot - Initial configuration
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
+# AVB - System & Product
 BOARD_AVB_VBMETA_SYSTEM := system product
 BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
+
+# AVB - Recovery
 BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
 BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
@@ -47,21 +53,32 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 # Assertation
 TARGET_OTA_ASSERT_DEVICE := RMX3151,RMX3286,spaced
 
-# Bootloader
+# Board platform configuration
 TARGET_BOARD_PLATFORM := mt6781
+
+# Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := oppo6781
 TARGET_NO_BOOTLOADER := true
 
-# Build system
+# Build system flags
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
-# Filesystem
+# FS config (required for vendor_overlay)
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
-# Kernel
+# Kernel - CMDLINE
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+
+# Kernel - compilation configuration
+TARGET_KERNEL_CONFIG := spaced_defconfig
+TARGET_KERNEL_SOURCE := kernel/realme/spaced
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := r383902
 TARGET_KERNEL_ADDITIONAL_FLAGS := HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+
+# Kernel - Offsets and Base configuration
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_PAGESIZE := 2048
@@ -69,17 +86,14 @@ BOARD_KERNEL_TAGS_OFFSET := 0x0bc08000
 BOARD_RAMDISK_OFFSET := 0x07c08000
 BOARD_DTB_OFFSET := 0x0bc08000
 
+# Kernel - Seperate DTBO
 BOARD_KERNEL_SEPARATED_DTBO := false
-BOARD_BOOT_HEADER_VERSION := 2
-
-BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 
-TARGET_KERNEL_CONFIG := spaced_defconfig
-TARGET_KERNEL_SOURCE := kernel/realme/spaced
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := r383902
+# Boot - Header version configuration
+BOARD_BOOT_HEADER_VERSION := 2
 
+# MKBOOTIMG - Arguments
 BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
 BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
@@ -88,40 +102,46 @@ BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
-# HIDL
-DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/framework_manifest.xml
-
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 BOARD_ROOT_EXTRA_FOLDERS += metadata
 
-# Partitions
+# Partitions - sizes
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
 BOARD_CACHEIMAGE_PARTITION_SIZE := 452984832
 BOARD_DTBOIMG_PARTITION_SIZE := 8388608
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 134217728
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 115888586752
+
+# Partitions - Filesystems
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Prebuilt vendor configuration
 BUILD_WITHOUT_VENDOR := true
+
+# Copy out directories
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_PRODUCT := product
 
+# Partitions - Reserved sizes
 BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT := -1
 BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 1048576000
 BOARD_PRODUCTIMAGE_EXTFS_INODE_COUNT := -1
 BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 1048576000
 
-# Partitions (Dynamic)
+# Don't build SYSTEM_ROOT image
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
+
+# Partitions (Dynamic)
 BOARD_MAIN_SIZE := 10198450176
 BOARD_MAIN_PARTITION_LIST := system product
 BOARD_SUPER_PARTITION_SIZE := 10200547328
 BOARD_SUPER_PARTITION_GROUPS := main
 
-# Power
+# Power - D2TW node path configuration
 TARGET_TAP_TO_WAKE_NODE := /proc/touchpanel/double_tap_enable
 
 # Recovery
